9981923a4a64415798677724686d69d6e44eecf3
zhebinhu <huzhebin2017@gmail.com>
2018-04-22 13:52:17 +0800

    Initial commit

---
+++ src/main/java/com/huzb/loadbalancing/SendController.java
@@ -0,0 +1,14 @@
+package com.huzb.loadbalancing;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author huzb
+ * @version v1.0.0
+ * @date 2018/4/21
+ */
+
+@RestController
+public class SendController {
+
+}
